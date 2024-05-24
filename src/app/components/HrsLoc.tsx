2b5985faeb5a68338a1fb9ee97aee70cbb829a3e
zorenal <zorenzal@gmail.com>
2024-05-23 21:46:52 -0700

    reworked images

--- src/app/components/HrsLoc.tsx
+++ src/app/components/HrsLoc.tsx
@@ -65,96 +65,96 @@ export default function HrsnLoc() {
 
   return (
     <section
-      id="Hours&Location"
-      className="flex flex-col sm:h-auto justify-center sm:m-5"
-    >
-      {/* Information section */}
-      <div className="flex flex-col items-center border border-dotted border-black w-full overflow-hidden">
+    id="Hours&Location"
+    className="flex flex-col sm:h-auto justify-center"
+  >
+    {/* Information section */}
+    <div className="flex flex-col items-center w-full overflow-hidden">
+      <div className="flex flex-col items-center w-full">
+        <p className="text-2xl sm:text-3xl font-bold mb-5 mt-5">Visit Us</p>
         <div className="flex flex-col items-center w-full">
-          <p className="text-2xl sm:text-3xl font-bold mb-5 mt-5">Visit Us</p>
-          <div className="flex flex-col items-center w-full">
-            {locations.map((location, index) => (
-              <button
-                key={index}
-                onClick={() => handleLocationClick(index)}
-                className={`w-auto mb-3 border rounded-lg p-2 ${
-                  selectedLocation === index
-                    ? "bg-black bg-opacity-10 font-bold"
-                    : ""
-                }`}
-              >
-                <span className="text-sm sm:text-base">{location.name}</span>
-              </button>
-            ))}
-          </div>
           {locations.map((location, index) => (
-            <div
+            <button
               key={index}
-              className={`flex transition-all duration-1000 ease-in-out ${
-                selectedLocation === index
-                  ? "max-h-screen opacity-100"
-                  : "max-h-0 opacity-0"
+              onClick={() => handleLocationClick(index)}
+              className={`w-auto mb-3 border rounded-lg p-2 ${
+                selectedLocation === index ? "bg-black bg-opacity-10 font-bold" : ""
               }`}
             >
-              <div className="flex flex-col items-center w-full m-8">
-                <p className="text-lg sm:text-xl font-bold mb-3">
-                  {location.name}
-                </p>
-                <p className="text-sm sm:text-base mb-3 underline text-center">
-                  <Link
-                    href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
-                      location.address
-                    )}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {location.address}
-                  </Link>
-                </p>
-                <p className="text-sm sm:text-base mb-3">
-                  <Link href={`tel:${location.phone}`} className="underline">
-                    {location.phone}
-                  </Link>
-                </p>
-                <div className="mb-8 text-center">
-                  {Object.entries(location.hours).map(([day, hours]) => (
-                    <div key={day} className="text-sm sm:text-base mb-2">
-                      <p className="font-bold">{day}</p>
-                      <p>{hours}</p>
-                    </div>
-                  ))}
-                </div>
-                <p className="font-bold text-center">Happy Hour</p>
-                <p className="text-sm sm:text-base">{location.happyHour}</p>
-                <button className="mt-8 px-6 py-3 bg-red-600 text-white text-lg font-bold rounded">
-                  Reserve A Table
-                </button>
-              </div>
-            </div>
+              <span className="text-sm sm:text-base">{location.name}</span>
+            </button>
           ))}
         </div>
-      </div>
-
-
-      {/* Locatin Images*/}
-      <div className="w-screen">
         {locations.map((location, index) => (
           <div
             key={index}
-            className={`flex justify-center transition-all duration-1000 ease-in-out ${
-              selectedLocation === index
-                ? "h-1/2 opacity-100"
-                : "max-h-0 opacity-0"
+            className={`flex transition-all duration-1000 ease-in-out ${
+              selectedLocation === index ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
             }`}
           >
-            <img
-              src={`${location.image}`}
-              alt="Location Image"
-              className="w-full object-cover h-40 sm:h-96"
-            />
+            <div className="flex flex-col items-center w-full m-8">
+              <p className="text-lg sm:text-xl font-bold mb-3">{location.name}</p>
+              <p className="text-sm sm:text-base mb-3 underline text-center">
+                <Link
+                  href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+                    location.address
+                  )}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {location.address}
+                </Link>
+              </p>
+              <p className="text-sm sm:text-base mb-3">
+                <Link href={`tel:${location.phone}`} className="underline">
+                  {location.phone}
+                </Link>
+              </p>
+              <div className="mb-8 text-center">
+                {Object.entries(location.hours).map(([day, hours]) => (
+                  <div key={day} className="text-sm sm:text-base mb-2">
+                    <p className="font-bold">{day}</p>
+                    <p>{hours}</p>
+                  </div>
+                ))}
+              </div>
+              <p className="font-bold text-center">Happy Hour</p>
+              <p className="text-sm sm:text-base">{location.happyHour}</p>
+              <button className="mt-8 px-6 py-3 bg-red-600 text-white text-lg font-bold rounded">
+                Reserve A Table
+              </button>
+            </div>
           </div>
         ))}
       </div>
-    </section>
+    </div>
+  
+    {/* Location Images */}
+    <div className="w-screen">
+      {locations.map((location, index) => (
+        <div
+          key={index}
+          className={`flex justify-center transition-all duration-1000 ease-in-out ${
+            selectedLocation === index ? "h-1/2 opacity-100" : "max-h-0 opacity-0"
+          }`}
+        >
+          <img
+            src={`${location.image || 'rstock.jpg'}`}
+            alt="Location Image"
+            className="w-full object-cover h-40 sm:h-96"
+          />
+        </div>
+      ))}
+      {!locations.some((location, index) => selectedLocation === index) && (
+        <div className="flex justify-center transition-all duration-1000 ease-in-out h-1/2 opacity-100">
+          <img
+            src="rstock.jpg"
+            alt="Default Location Image"
+            className="w-full object-cover h-40 sm:h-96"
+          />
+        </div>
+      )}
+    </div>
+  </section>
   );
 }
